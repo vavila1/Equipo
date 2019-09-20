ce7642486cd8281c95cea6fc668ebf3e9ef9c464
JorgeLpz <A01209043@itesm.mx>
2019-09-20 00:59:43 -0500

    'Login'

---
+++ Proyecto/Login/loginScript.js
@@ -0,0 +1,8 @@
+
+// ToolTip
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.tooltipped');
+    var instances = M.Tooltip.init(elems, options);
+  });
+
+
